c9170d6d5798ae996fe50fe3bf8b36a2729f5e58
mayeskhatri243 <mayeskhatri243@gmail.com>
2024-01-16 10:05:07 +0530

    COM: added stanger name if prompt is empty

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -12,7 +12,7 @@ export const db = firebase;
 
 var firepadRef = firebase.database().ref();
 
-export const userName = prompt("What's your name?");
+export const userName = prompt("What's your name?") || "Stanger";
 const urlparams = new URLSearchParams(window.location.search);
 const roomId = urlparams.get("id");
 
